354aee761606b60d9faf3aaaf41117854e3cec77
Danielli <dannyteixeira955@gmail.com>
2019-04-30 15:05:02 -0300

    Verificação de Email V2

--- vemail.php
+++ vemail.php
@@ -5,7 +5,7 @@
 	$sql = "UPDATE usuario SET validada = 1 WHERE ident = ". $_SESSION["usuario"];
 	
 	mysqli_query($conexao, $sql);
-		
-	header("Location:index.php");
 
 ?>
+
+	<p> E-mail validado com sucesso! <a href = "index.php">Clique aqui</a> para voltar á página principal! </p>
